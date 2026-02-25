a462569c780012a07dcabea3044b23e034eb7726
Aqecdl <a_yessentay@kbtu.kz>
2026-02-25 11:21:16 +0500

    lab5 online store

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,34 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { Category } from './models/category.model';
+import { Product } from './models/product.model';
+import { CATEGORIES } from './models/categories.data';
+import { PRODUCTS } from './models/products.data';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, ProductListComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App {
-  protected readonly title = signal('lab5');
+export class AppComponent {
+  categories: Category[] = CATEGORIES;
+  allProducts: Product[] = PRODUCTS;
+
+  selectedCategoryId: number | null = null;
+
+  get filteredProducts(): Product[] {
+    if (this.selectedCategoryId === null) return [];
+    return this.allProducts.filter(p => p.categoryId === this.selectedCategoryId);
+  }
+
+  selectCategory(id: number) {
+    this.selectedCategoryId = id;
+  }
+
+  getCategoryName(): string {
+    return this.categories.find(c => c.id === this.selectedCategoryId)?.name ?? '';
+  }
 }
